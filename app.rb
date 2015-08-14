2651a53fba3da6d7669ba67d3e72c802fe0ec26e
chansuke <moonset20@gmail.com>
2015-08-15 03:20:15 +0900

    update

---
+++ app.rb
@@ -0,0 +1,73 @@
+require 'sinatra'
+require 'sinatra-websocket'
+require 'json'
+
+set :server, 'thin'
+set :sockets, []
+
+get '/' do
+  if !request.websocket?
+    slim :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
+
+#post '/log', provides: :json  do
+#  params = JSON.parse request.body.read
+#end
+
+post '/logs', provides: :json  do
+  params = JSON.parse(request.body.read)
+  p params
+  EM.next_tick { settings.sockets.each}
+end
+
+__END__
+@@ index
+<html>
+  <body>
+     <h1>Simple Echo & Chat Searver</h1>
+     <form id="form">
+       <input type="text" id="input" value="send a message"></input>
+     </form>
+     <div id="msgs"></div>
+  </body>
+
+  <script type="text/javascript">
+    window.onload = function(){
+      (function(){
+        var show = function(el){
+          return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
+        }(document.getElementById('msgs'));
+
+        var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
+        ws.onopen    = function()  { show('websocket opened'); };
+        ws.onclose   = function()  { show('websocket closed'); }
+        ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
+
+        var sender = function(f){
+          var input     = document.getElementById('input');
+          input.onclick = function(){ input.value = "" };
+          f.onsubmit    = function(){
+            ws.send(input.value);
+            input.value = "send a message";
+            return false;
+          }
+        }(document.getElementById('form'));
+      })();
+    }
+  </script>
+</html>
