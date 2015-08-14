aa9170f23969b92919a5f4394d71038d5cedc59d
chansuke <moonset20@gmail.com>
2015-08-14 22:16:41 +0900

    add websockets

--- article.rb
+++ article.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
+require 'sinatra-websocket'
 require 'json'
 
 get '/' do
-  slim :index
+  if !request.websocket?
+    slim :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
 
 post '/log', provides: :json  do
