b7b969f3e5004b679aee1e3332915fd7c086e9fd
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2015-08-16 22:22:17 +0900

    add websocket status

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,17 @@
 window.onload = function(){
   (function(){
-    var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
-    ws.onopen    = function()  { console.log('websocket opened'); };
-    ws.onclose   = function()  { console.log('websocket closed'); }
-    ws.onmessage = function(m) { console.log(m) };
+    var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
+    ws.onopen = function() {
+      console.log('websocket opened');
+      $('#websocket-status').text('websocket opened');
+    };
+    ws.onclose = function() {
+      console.log('websocket closed');
+      $('#websocket-status').text('websocket closed');
+    }
+    ws.onmessage = function(m) {
+      console.log(m)
+    };
   })();
 }
 
